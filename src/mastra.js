b090fbff3549d0d1b4cd7ca8cddc9bc85fc75450
Trilvid <trillztech34@gmail.com>
2025-11-06 23:06:34 +0100

    finial corrections made

--- src/mastra.js
+++ src/mastra.js
@@ -3,12 +3,12 @@ import { Mastra } from '@mastra/core';
 import { OpenAI } from 'openai';
 import { dailyPulseAgent } from './agents/dailyPulseAgent.js';
 
-const key = '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'
+// const key = 
 
 // Initialize OpenAI client
 const openai = new OpenAI({
-  // apiKey: process.env.OPENAI_API_KEY,
-  apiKey: key,
+  apiKey: process.env.OPENAI_API_KEY,
+  // apiKey: key,
 });
 
 export const mastra = new Mastra({
